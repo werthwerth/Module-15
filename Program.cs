6e069ae2c02e8d58cb95c6d1e766350724e90cc9
werthwerth <teleperedozik@mail.ru>
2023-08-21 12:55:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            // ???
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
